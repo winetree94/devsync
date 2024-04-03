97ed5a44bbcbe848105aeb5c52c4ff59e47cc9a1
winetree94 <winetree94@gmail.com>
2024-04-04 00:01:55 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import path from "path";
+import sample from "./sample.json";
+
+import { program } from "commander";
 
 console.log(path);
+
+console.log(sample);
+
+program
+  .name("devsync")
+  .option("-v, --version", "Output the version number", "0.0.1")
+  .description("A tool to sync your development environment");
+
+program.parse(process.argv);
