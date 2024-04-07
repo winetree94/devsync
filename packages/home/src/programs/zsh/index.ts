04e6592de69b9ad417474093e51941162b2945c8
winetree94 <winetree94@gmail.com>
2024-04-07 18:23:40 +0900

    update

--- packages/home/src/programs/zsh/index.ts
+++ packages/home/src/programs/zsh/index.ts
@@ -2,21 +2,11 @@ import { DevSyncAppConfig } from '@devsynk/core';
 
 export const ZSHConfig: DevSyncAppConfig = {
   appName: 'zsh',
-  git: [
-    // {
-    //   owner: "mattmc3",
-    //   repo: "antidote",
-    //   path: "~/.antidote",
-    // }
-  ],
+  git: [],
   files: [
     {
       source: './src/programs/zsh/.zshrc',
       target: '~/.zshrc',
     },
-    // {
-    //   source: "./src/user/programs/zsh/.zsh_plugins.txt",
-    //   target: "~/.zsh_plugins.txt",
-    // },
   ],
 };
